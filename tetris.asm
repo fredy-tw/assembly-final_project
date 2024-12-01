e9f4e5efc867df59084916ca52cd3bff8fb5b37b
fredy <fredy2268277@gmail.com>
2024-12-02 01:24:40 +0800

    可以畫出玩家方塊了

--- tetris.asm
+++ tetris.asm
@@ -1,22 +1,61 @@
 INCLUDE Irvine32.inc
+main          EQU start@0
+Drawplayer PROTO,,player:PTR BYTE, block_type:BYTE, xpos:byte, ypos:byte, direction:byte
+Draw PROTO
 .data
-xpos BYTE 10
-ypos BYTE 10
+xpos_1 BYTE 5
+ypos_1 BYTE 1
+xpos_2 BYTE 5
+ypos_2 BYTE 1
 inputChar BYTE ?
 isJumping BYTE ?
 player_1 Byte 20 dup('..........',0)
 empty Byte '             ',0
 player_2 Byte 20 dup('..........',0)
-main          EQU start@0
 .code
 main PROC
     ;mov eax,green+(blue*16) ;設定顏色 背景藍色 方塊可以隨便改
     call SetTextColor
-    call Draw
+    gameloop:
+    invoke Drawplayer,OFFSET [player_1] ,'f' ,xpos_1 ,ypos_1 ,1
+    invoke Draw
     call ReadChar
+    jmp gameloop
     exit
 main ENDP
+Drawplayer PROC,player:PTR BYTE, block_type:BYTE, xpos:byte, ypos:byte, direction:byte
+    mov edx,player
+    mov eax,0
+    mov al,ypos
+    mov bl,11
+    mul bl
+    add al,xpos
+    add edx,eax
+    mov BYTE PTR [edx],'X'
+    ; .IF block_type=='I'
+    ;     .IF direction==1  ;| center is at bottom 2
 
+    ;     .ENDIF
+    ;     .IF direction==2  ;___ center is at right 2
+
+    ;     .ENDIF
+    ; .ENDIF
+    ; .IF block_type=='O'
+
+    ; .ENDIF
+    ; .IF block_type=='T'
+
+    ; .ENDIF
+    ; .IF block_type=='S'
+
+    ; .ENDIF
+    ; .IF block_type=='Z'
+
+    ; .ENDIF
+    ; .IF block_type=='J'
+
+    ; .ENDIF
+Drawplayer ENDP
 Draw PROC
     mov ecx,20
     mov dl,3;xpos
