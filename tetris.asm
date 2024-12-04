0819983a5972be297f645399f17d4d79c490b973
fredy <fredy2268277@gmail.com>
2024-12-04 23:47:50 +0800

    除了I型方塊外 在指定位置畫出方塊的函數皆已完成

--- tetris.asm
+++ tetris.asm
@@ -1,29 +1,40 @@
 INCLUDE Irvine32.inc
 main          EQU start@0
-Drawplayer PROTO,,player:PTR BYTE, block_type:BYTE, xpos:byte, ypos:byte, direction:byte
+Drawplayer PROTO,player:PTR BYTE, block_type:BYTE, xpos:byte, ypos:byte, direction:byte,paint:byte
+Rotate_block PROTO,player:PTR BYTE,block_type:BYTE,xpos:byte,ypos:byte,direction:byte
 Draw PROTO
 .data
-xpos_1 BYTE 5
+xpos_1 BYTE 4
 ypos_1 BYTE 1
-xpos_2 BYTE 5
+xpos_2 BYTE 4
 ypos_2 BYTE 1
 inputChar BYTE ?
 isJumping BYTE ?
-player_1 Byte 20 dup('..........',0)
+player_1type byte ?;indicate what kind of block player is controling I O J L S Z T
+player_2type byte ? 
+generate_1 byte 1
+generate_2 byte 1
+player_1 Byte 22 dup('..........',0);多出來的兩格是 給一開始方塊的位置
 empty Byte '             ',0
-player_2 Byte 20 dup('..........',0)
+player_2 Byte 22 dup('..........',0)
 .code
 main PROC
     ;mov eax,green+(blue*16) ;設定顏色 背景藍色 方塊可以隨便改
     call SetTextColor
     gameloop:
-    invoke Drawplayer,OFFSET [player_1] ,'f' ,xpos_1 ,ypos_1 ,1
+    .IF player_1==1
+    .ENDIF
+    invoke Drawplayer,OFFSET [player_1] ,'J' ,xpos_1 ,ypos_1,4,'X'
+    ; invoke Drawplayer,OFFSET [player_1] ,'O' ,xpos_1 ,ypos_1,1,'x'
     invoke Draw
     call ReadChar
     jmp gameloop
+    call ReadChar
     exit
 main ENDP
-Drawplayer PROC,player:PTR BYTE, block_type:BYTE, xpos:byte, ypos:byte, direction:byte
+Generate_block PROC
+Generate_block ENDP
+Rotate_block PROC,player:PTR BYTE,block_type:BYTE,xpos:byte,ypos:byte,direction:byte
     mov edx,player
     mov eax,0
     mov al,ypos
@@ -31,33 +42,250 @@ Drawplayer PROC,player:PTR BYTE, block_type:BYTE, xpos:byte, ypos:byte, directio
     mul bl
     add al,xpos
     add edx,eax
-    mov BYTE PTR [edx],'X'
-    ; .IF block_type=='I'
-    ;     .IF direction==1  ;| center is at bottom 2
+Rotate_block ENDP
+Drawplayer PROC,player:PTR BYTE, block_type:BYTE, xpos:byte, ypos:byte, direction:byte,paint:byte;最後一個參數使我們可以決定畫什麼方塊
+    mov edx,player
+    mov eax,0
+    mov al,ypos
+    mov bl,11
+    mul bl
+    add al,xpos
+    add edx,eax
+    mov al,paint
+    .IF block_type=='I' ;not good 
+        .IF direction==1 ;left center 2
+        .ENDIF
+        .IF direction==2  ;up 2
+        .ENDIF
+        .IF direction==3 ;right 2
 
-    ;     .ENDIF
-    ;     .IF direction==2  ;___ center is at right 2
+        .ENDIF
+        .IF direction==4 ;down 2
 
-    ;     .ENDIF
-    ; .ENDIF
-    ; .IF block_type=='O'
-
-    ; .ENDIF
-    ; .IF block_type=='T'
-
-    ; .ENDIF
-    ; .IF block_type=='S'
-
-    ; .ENDIF
-    ; .IF block_type=='Z'
-
-    ; .ENDIF
-    ; .IF block_type=='J'
-
-    ; .ENDIF
+        .ENDIF
+    .ENDIF
+    .IF block_type=='O' ; good
+        mov BYTE PTR [edx],al
+        inc edx
+        mov BYTE PTR [edx],al
+        sub edx,11
+        mov BYTE PTR [edx],al
+        dec edx
+        mov BYTE PTR [edx],al
+    .ENDIF
+    .IF block_type=='T' ;good
+        .IF direction==1 ;face up
+            sub edx,11
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==2  ;face right
+            sub edx,11
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==3 ;face down
+            dec edx
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al 
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==4 ;face left
+            sub edx,11
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+        .ENDIF
+    .ENDIF
+    .IF block_type=='S' ;good
+        .IF direction==1 
+            sub edx,11
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,9
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==2  
+            sub edx,11
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al 
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==3
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,9
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==4
+            sub edx,12
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+        .ENDIF
+    .ENDIF
+    .IF block_type=='Z' ;good
+        .IF direction==1 
+            sub edx,12
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==2  
+            sub edx,10
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==3
+            dec edx
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==4
+            sub edx,11
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al 
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+        .ENDIF
+    .ENDIF
+    .IF block_type=='L' ;good
+        .IF direction==1
+            sub edx,12
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==2  
+            sub edx,11
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==3
+            dec edx
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al 
+            add edx,11
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==4
+            sub edx,11
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+            add edx,10
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+    .ENDIF
+    .IF block_type=='J' ;good
+        .IF direction==1
+            sub edx,10
+            mov BYTE PTR [edx],al
+            add edx,9
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al 
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==2  
+            sub edx,11
+            mov BYTE PTR [edx],al 
+            add edx,11
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al 
+            inc edx
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==3
+            dec edx
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,9
+            mov BYTE PTR [edx],al
+        .ENDIF
+        .IF direction==4
+            sub edx,12
+            mov BYTE PTR [edx],al
+            inc edx
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+            add edx,11
+            mov BYTE PTR [edx],al
+        .ENDIF
+    .ENDIF
+    ret
 Drawplayer ENDP
 Draw PROC
-    mov ecx,20
+    mov ecx,22
     mov dl,3;xpos
     mov dh,3;ypos
     mov ebx,0
